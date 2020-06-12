9634736e51845bcd6293aa539be8036872e7b813
khoi <9buianhkhoi@gmail.com>
2020-06-12 23:17:22 +0700

    Add python imgtotext libs

--- Server/Python/img2text.py
+++ Server/Python/img2text.py
@@ -1,3 +1,9 @@
-﻿def img2text(imgPath = ""):
-	print("abc")
-	return 'abc'
+﻿from pytesseract.scr import pytesseract
+from Pillow.scr.PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\LONG\\AppData\\Local\\Tesseract-OCR\\tesseract.exe'
+
+def imgTotext(imgPath = ""):
+	open_image = Image.open(imgPath)
+	text = pytesseract.image_to_string(open_image)
+	return text
